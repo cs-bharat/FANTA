913601cd3d8ad1d6540df3434fc68ca8a84c34d7
Bharat prajapati <bharatkumarm413@gmail.com>
2024-02-24 00:13:18 +0530

    gsap animations

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ let tl = gsap.timeline({
         triger:'.two', // selector
         start:'10% 20%',  // scroll start level
         end:'60% 90%', // scroll start level
-        scrub : true,
-        markers: true 
+        scrub : true, // reverse animation deta hellk
+        markers: false
     }
 });
 
@@ -39,10 +39,41 @@ tl.to('#fanta',{
 },'one')
 .to('#orange-cut',{
     scale:'1.5',
-    top:'110%',
-    left:'15%'
+    top:'170%',
+    left:'25%'
 },'one')
 .to('#orange',{
     top:'175%',
     left:'85%'
+},'one')
+
+// ---three---//
+let tl2 = gsap.timeline({
+    scrollTrigger:{
+        triger:'.three', // selector
+        start:'0% 40%',  // scroll start level
+        end:'90% 70%', // scroll start level
+        scrub : true, // reverse animation deta hellk
+        markers: true 
+    }
+});
+
+tl2.from('#cocacola',{
+    rotate: "-180deg",
+    top:"190%",
+  
+},'two')
+tl2.from('#pepsi',{
+    rotate: "60deg",
+    top:"200%",
+},'two')
+
+tl2.to('#fanta',{
+    top:'200%',
+    left:'30%'
+},'one')
+
+tl2.to('#orange-cut',{
+    top:'180%',
+    left:'45%'
 },'one')
